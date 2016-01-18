809fb1fdbf76f09393d922c71d16aebd6a4277e8
Geovani de Souza <geovanisouza92@gmail.com>
2016-01-18 15:09:01 -0200

    [WIP] Adding runtime drivers

---
+++ driver/docker/driver.go
@@ -0,0 +1,78 @@
+// Runtime driver implementation for Docker
+package docker
+
+import (
+	"strings"
+
+	"github.com/fsouza/go-dockerclient"
+	"github.com/geovanisouza92/lambdac/driver"
+	"github.com/geovanisouza92/lambdac/types"
+)
+
+func init() {
+	driver.Register("docker", new(dockerDriver))
+}
+
+type dockerDriver struct {
+	c *docker.Client
+}
+
+func (d *dockerDriver) Init(options []string) (err error) {
+	optionsMap := map[string]string{}
+	for _, o := range options {
+		pair := strings.Split(o, "=")
+		optionsMap[pair[0]] = pair[1]
+	}
+
+	host := optionsMap["DOCKER_HOST"]
+	if host == "" {
+		host = "unix:///var/run/docker.sock"
+	}
+	d.c, err = docker.NewClient(host)
+	return
+}
+
+func (d *dockerDriver) CreateInstance(function types.Function, runtime types.Runtime) (id string, err error) {
+	opts := docker.CreateContainerOptions{
+		// Name: "",
+		Config: &docker.Config{
+			Memory:     int64(1024 * 1024 * function.Memory),
+			Env:        []string{},
+			Entrypoint: []string{}, // Must override?
+			Cmd:        []string{runtime.Command},
+			Image:      runtime.Image,
+			// WorkingDir
+			// TODO Labels
+		},
+		HostConfig: &docker.HostConfig{
+		// RestartPolicy (runtime.Agent)
+		},
+	}
+
+	if runtime.Agent {
+		opts.HostConfig.RestartPolicy = docker.RestartPolicy{Name: "unless-stopped"}
+	}
+
+	container, err := d.c.CreateContainer(opts)
+	if err != nil {
+		return
+	}
+	id = container.ID
+
+	return
+}
+
+func (d *dockerDriver) StartInstance(id string) (err error) {
+	// TODO
+	return
+}
+
+func (d *dockerDriver) StopInstance(id string) (err error) {
+	// TODO
+	return
+}
+
+func (d *dockerDriver) DestroyInstance(id string) (err error) {
+	// TODO
+	return
+}
---
+++ driver/driver.go
@@ -0,0 +1,49 @@
+// Runtime driver abstraction
+package driver
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/geovanisouza92/lambdac/types"
+)
+
+var (
+	driversMu sync.Mutex
+	drivers   = make(map[string]Driver)
+)
+
+type Driver interface {
+	// Configure driver
+	Init(options []string) error
+	// Create a function instance
+	CreateInstance(function types.Function, runtime types.Runtime) (string, error)
+	// Start a function instance
+	StartInstance(id string) error
+	// Stop a function instance
+	StopInstance(id string) error
+	// Destroy a function instance
+	DestroyInstance(id string) error
+}
+
+func Register(name string, driver Driver) {
+	driversMu.Lock()
+	defer driversMu.Unlock()
+	if driver == nil {
+		panic("driver: Register driver is nil")
+	}
+	if _, dup := drivers[name]; dup {
+		panic("driver: Register called twice for driver " + name)
+	}
+	drivers[name] = driver
+}
+
+func Open(name string) (Driver, error) {
+	driversMu.Lock()
+	driver, ok := drivers[name]
+	driversMu.Unlock()
+	if !ok {
+		return nil, fmt.Errorf("driver: unknown driver %q (forgotten import?)", name)
+	}
+	return driver, nil
+}
