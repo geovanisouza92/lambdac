b089db9b888b22616f0958b536dde6f918c4a051
Geovani de Souza <geovanisouza92@gmail.com>
2016-01-18 15:26:49 -0200

    Adding function type

--- types/types.go
+++ types/types.go
@@ -5,6 +5,23 @@ import (
 	"time"
 )
 
+type Function struct {
+	ID          string    `json:"id,omitempty"`
+	Name        string    `json:"name,omitempty"`
+	Runtime     string    `json:"runtime,omitempty"`
+	Handler     string    `json:"handler,omitempty"`
+	Description string    `json:"description,omitempty"`
+	Code        string    `json:"code,omitempty"`
+	Timeout     int64     `json:"timeout,omitempty"`
+	Memory      int       `json:"memory,omitempty"`
+	Instances   int       `json:"instances,omitempty"`
+	Env         []string  `json:"env,omitempty"`
+	Created     time.Time `json:"created,omitempty"`
+	Updated     time.Time `json:"updated,omitempty"`
+}
+
+type Functions []Function
+
 type Runtime struct {
 	ID      string    `json:"id,omitempty"`
 	Name    string    `json:"name,omitempty"`
